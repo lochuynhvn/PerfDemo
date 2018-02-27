af1ed206529b0b7bcb01d688677fec0258b8663d
lochuynhvn <huynhhuulocvn@gmail.com>
2018-02-27 16:02:07 +0700

    First commit

---
+++ src/main/java/com/example/jmeter_testproject/App.java
@@ -0,0 +1,13 @@
+package com.example.jmeter_testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
