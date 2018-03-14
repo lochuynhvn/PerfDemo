67fb6a65e87643295860a84ac98267165e33916e
lochuynhvn <huynhhuulocvn@gmail.com>
2018-03-14 17:42:53 +0700

    new package

---
+++ performance_core/src/com/test/Test.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import com.example.Currency;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Currency currency = new Currency();
+		int limit = currency.getLimit();
+		System.out.println(limit);
+		
+	}
+
+}
