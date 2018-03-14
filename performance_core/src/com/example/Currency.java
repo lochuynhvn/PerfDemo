4a44f0da33c44e800bb61f31f50d2789d6164b29
lochuynhvn <huynhhuulocvn@gmail.com>
2018-03-14 17:42:53 +0700

    new package

---
+++ performance_core/src/com/example/Currency.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.Random;
+
+public class Currency {
+public int getLimit() {
+	Random rd = new Random();
+	int limit = rd.nextInt(10);
+	return limit;
+}
+
+}
